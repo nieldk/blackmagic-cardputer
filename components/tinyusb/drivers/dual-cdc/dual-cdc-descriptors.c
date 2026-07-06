c1caf144c618fcf956046a8fff34e4562dcdde43
nieldk <nieldk@noreply.codeberg.org>
2026-07-06 13:08:04 +0200

    Update components/tinyusb/drivers/dual-cdc/dual-cdc-descriptors.c

--- components/tinyusb/drivers/dual-cdc/dual-cdc-descriptors.c
+++ components/tinyusb/drivers/dual-cdc/dual-cdc-descriptors.c
@@ -150,10 +150,10 @@ uint8_t const blackmagic_desc_hs_configuration[] = {
 // array of pointer to string descriptors
 static char* blackmagic_string_desc[] = {
     (char[]){0x09, 0x04}, // 0: is supported language is English (0x0409)
-    "Litui", // 1: Manufacturer
+    "Niel Nielsen", // 1: Manufacturer
     "Blackmagic ESP32", // 2: Product
     "blackmagic", // 3: Serials, should use chip ID
-    "Blackmagic ESP32", // 4: CDC Interface
+    "Blackmagic ESP32 CDC", // 4: CDC Interface
     "", // 5: MSC Interface
     "", // 6: HIDs
 };
